929d932aa2675db13891d6a0ff02c445c1a34361
Daniel Brahneborg <basic70@gmail.com>
2019-07-03 15:20:27 +0200

    path.join, arrow functions

--- src/routes/conf.js
+++ src/routes/conf.js
@@ -1,29 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var confs = require('../modules/conf');
+var path = require('path');
+var confs = require(path.join('..', 'modules', 'conf'));
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     confs.get((confs) => {
         res.render('index', {title: 'Conferences', confs });
     });
 });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', (req, res) => {
     confs.get_by_id(req.params.id,(c) => {
         console.log('show', c);
         res.render('conf', {title: 'Conference', conf: {id: c.id, name: c.name, acronym: c.acronym}})
     });
 });
 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', (req, res) => {
     console.log('delete conf', req.params.id);
     confs.delete(req.params.id, () => {
         res.redirect('/conf');
     })
 });
 
-router.post('/', function(req, res) {
+router.post('/', (req, res) => {
     console.log('post conf / with params', req.body);
     if (!req.body.empty) {
         confs.add(req.body.name, (doc) => {
