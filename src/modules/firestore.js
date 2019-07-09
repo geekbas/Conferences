6f123508670246941d8973a22f216d25bc951f7e
Daniel Brahneborg <basic70@gmail.com>
2019-07-09 15:59:28 +0200

    firestore: info in firebase-auth.json

--- src/modules/firestore.js
+++ src/modules/firestore.js
@@ -1,5 +1,5 @@
 var admin = require('firebase-admin');
-var serviceAccount = require("./conferences-77da0-firebase-adminsdk-eht10-e246d33a60.json");
+var serviceAccount = require("./firebase-auth.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://conferences-77da0.firebaseio.com"
