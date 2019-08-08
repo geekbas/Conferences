016f5c9c93f7d53f91dffc72df6bf4e3003002ca
Daniel Brahneborg <basic70@gmail.com>
2019-08-08 10:33:40 +0200

    no more firestore

--- modules/firestore.js
+++
@@ -1,8 +0,0 @@
-var admin = require('firebase-admin')
-var serviceAccount = JSON.parse(process.env.FIREBASE_AUTH)
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://conferences-77da0.firebaseio.com"
-})
-
-module.exports = admin.firestore()
