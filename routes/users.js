135fc8109facc0116d010e5ac533a523d4738602
Daniel Brahneborg <basic70@gmail.com>
2019-08-10 10:24:56 +0200

    unused User

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const Conf = require(path.join('..', 'modules', 'conf'))
-//const User = require(path.join('..', 'modules', 'user'))
 const Submission = require(path.join('..', 'modules', 'submission'))
 const Track = require(path.join('..', 'modules', 'track'))
 const helpers = require('./helpers')
